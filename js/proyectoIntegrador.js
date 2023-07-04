f64a7b07ec004ed4cebd4741aaac3d567e60b41d
liliana <lilianabiogalenic@gmail.com>
2023-07-04 16:55:04 -0300

    subiendo mi SIMULADOR DE PRESTAMOS 2DA PARTE

---
+++ js/proyectoIntegrador.js
@@ -0,0 +1,60 @@
+class Prestamo {
+    constructor(nombreCompleto, montoSolicitado, plazo, cuotaMensual) {
+        this.nombreCompleto = nombreCompleto;
+        this.montoSolicitado = montoSolicitado;
+        this.plazo = plazo;
+        this.cuotaMensual = cuotaMensual;
+    }
+}
+
+const prestamos = [];
+
+function simularPrestamo() {
+    const resultadoElement = document.getElementById("resultado");
+
+    let nombre = document.getElementById("nombre").value;
+    let monto = document.getElementById("monto").value;
+    let plazo = document.getElementById("plazo").value;
+
+    if (!nombre || !monto || !plazo) {
+        resultadoElement.textContent = "No ingresó los datos requeridos";
+        return;
+    } else if (monto <= 0 || plazo <= 0) {
+        resultadoElement.textContent = "El monto y el plazo deben ser mayores a cero.";
+        return;
+    }
+
+    let InteresAnual = 0.12;
+    let InteresMensual = InteresAnual / 12;
+    let cuotaMensual = monto * (InteresMensual / (1 - Math.pow(1 + InteresMensual, -plazo)));
+
+    // Instancia de la clase Prestamo
+    let prestamo = new Prestamo(nombre, monto, plazo, cuotaMensual.toFixed(2));
+
+    prestamos.push(prestamo); // OBJETO AGREGADO Y PUSHEADO
+
+    resultadoElement.textContent = "El monto a pagar mensualmente es: $" + cuotaMensual.toFixed(2);
+
+    console.table(prestamos);
+
+    // Guardar el historial de préstamos en localStorage como JSON
+    localStorage.setItem("prestamos", JSON.stringify(prestamos));
+
+    // Limpiar los campos de entrada
+    document.getElementById("nombre").value = "";
+    document.getElementById("monto").value = "";
+    document.getElementById("plazo").value = "";
+}
+
+function cargarHistorial() {
+    const prestamosGuardados = localStorage.getItem("prestamos");
+    if (prestamosGuardados) {
+        const prestamos = JSON.parse(prestamosGuardados);
+        console.table(prestamos);
+    } else {
+        console.log("No se encontró historial de préstamos.");
+    }
+}
+
+document.getElementById("simular").addEventListener("click", simularPrestamo);
+
